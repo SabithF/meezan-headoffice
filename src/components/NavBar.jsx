1d689fb8fd7d175533b7ac0ddb812be8dc006e8f
Sabithf <sabithfariq@gmail.com>
2025-07-26 11:27:41 +0100

    Nav bar update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -33,19 +33,21 @@ function NavBar() {
         <nav
             ref={navRef}
             className={`fixed top-0 left-1/2 transform -translate-x-1/2 z-50 transition-all duration-300 ${scrolled
-                    ? "bg-blue-800/35 backdrop-blur-sm shadow-xl rounded-xl px-6 py-2 top-6 w-[90%] md:w-[80%]"
-                    : "w-full left-0 bg-white/20 backdrop-blur-sm text-white px-4 py-3"
+                ? "bg-blue-800/35 backdrop-blur-sm shadow-xl rounded-xl px-6 py-2 top-6 w-[90%] md:w-[80%]"
+                : "w-full left-0  text-white px-4 py-3"
                 }`}
         >
-            <div className="flex items-center justify-between max-w-screen-xl mx-auto">
+            <div className="flex items-center  justify-between md:justify-start max-w-screen-xl mx-auto">
                 {/* Logo */}
                 <a href="/">
                     <img
                         src="/assets/img/meez-logo.png"
                         alt="Logo"
-                        className="h-14 py-4 flex justify-center items-center"
+                        className="h-14 py-4 flex justify-center items-center md:pr-3"
                     />
                 </a>
+
+                <div className=" hidden md:block py-3 border border-white " />
 
                 {/* Hamburger button */}
                 <button
@@ -64,20 +66,21 @@ function NavBar() {
                         />
                     </svg>
                 </button>
-
                 {/* Menu items */}
                 <div
-                    className={`${isNavOpen ? "block" : "hidden"
-                        } md:flex md:items-center md:space-x-6 absolute md:static top-full left-0 rounded-xl mt-3 md:mt-0 w-full md:w-auto bg-slate-800 text-white md:bg-transparent text-sm font-semibold uppercase md:text-inherit mx-1 md:mx-0 py-10 px-5 md:p-0 text-center`}
+                    className={`${isNavOpen ? "block" : "hidden"} 
+    md:flex md:items-center md:justify-between absolute md:static top-full right-3 rounded-xl mt-3 md:pl-3 md:mt-0  
+    md:w-full bg-slate-800 text-white md:bg-transparent 
+    text-sm font-semibold uppercase md:text-inherit w-60 md:mx-0 py-10 px-5 md:p-0 text-center`}
                 >
-                    <ul className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-4 md:text-white text-center">
+                    {/* Left menu items */}
+                    <ul className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6 md:text-white text-center">
                         <li>
                             <a href="/" className="hover:text-blue-300">
                                 Home
                             </a>
                         </li>
 
-                        {/* Dropdown */}
                         <li className="relative items-center justify-center text-center">
                             <button
                                 onClick={() => setIsDropdownOpen(!isDropdownOpen)}
@@ -89,11 +92,14 @@ function NavBar() {
                                 </svg>
                             </button>
                             {isDropdownOpen && (
-                                <div className="absolute top-full mt-2 bg-white text-black shadow-lg p-4 z-50 w-64">
-                                    <ul className="space-y-2 text-sm">
-                                        <li><a href="#">Plantations</a></li>
-                                        <li><a href="#">Hardware</a></li>
-                                        <li><a href="#">Hospitality</a></li>
+                                <div className="absolute top-full mt-2 bg-white text-black shadow-lg p-4 z-50 w-64 rounded-lg right-3 md:left-6">
+                                    <ul className="space-y-2 text-sm ">
+                                        <li className="hover:border border-slate-400/30 rounded-lg py-3 hover:bg-blue-200/50"><a href="#">Plantations</a></li>
+                                        <li className="hover:border border-slate-400/30 rounded-lg py-3 hover:bg-blue-200/50" ><a href="#">Tea Factory</a></li>
+                                        <li className="hover:border border-slate-400/30 rounded-lg py-3 hover:bg-blue-200/50" ><a href="#">Hardware & Plantation Division</a></li>
+                                        <li className="hover:border border-slate-400/30 rounded-lg py-3 hover:bg-blue-200/50" ><a href="#">Hospitality</a></li>
+                                        <li className="hover:border border-slate-400/30 rounded-lg py-3 hover:bg-blue-200/50" ><a href="#">Education</a></li>
+                                        <li className="hover:border border-slate-400/30 rounded-lg py-3 hover:bg-blue-200/50" ><a href="#">Hospitality</a></li>
                                     </ul>
                                 </div>
                             )}
@@ -104,16 +110,21 @@ function NavBar() {
                                 News
                             </a>
                         </li>
-                        <li>
-                            <a
-                                href="/contact"
-                                className="bg-slate-900 p-3 text-white rounded-lg hover:text-blue-300"
-                            >
-                                Contact
-                            </a>
-                        </li>
                     </ul>
+
+                    {/* Contact button aligned right on large screens */}
+                    <div className="mt-6 md:mt-0 md:ml-auto">
+                        <a
+                            href="/contact"
+                            className="p-3 text-white hover:text-blue-300 border border-white rounded-full"
+                        >
+                            Contact
+                        </a>
+                    </div>
                 </div>
+                
+
+
             </div>
         </nav>
     );
